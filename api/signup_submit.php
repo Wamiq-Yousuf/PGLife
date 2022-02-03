45ef5fd4cbf9f8aa271f21f7deec5c8a8f15fe45
wamii <84937427+Wamiq-Yousuf@users.noreply.github.com>
2022-02-03 21:38:01 +0530

    Add files via upload

---
+++ api/signup_submit.php
@@ -0,0 +1,51 @@
+<?php
+require("../includes/database_connect.php");
+
+$full_name = $_POST['full_name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$password = sha1($password);
+$college_name = $_POST['college_name'];
+$gender = $_POST['gender'];
+
+$sql = "SELECT * FROM users WHERE email='$email'";
+$result = mysqli_query($conn, $sql);
+if (!$result) {
+	$response = array("success" => false, "message" => "Something went wrong!");
+    echo json_encode($response);
+    return;
+	// echo "Something went wrong!";
+	// exit;
+}
+
+$row_count = mysqli_num_rows($result);
+if ($row_count != 0) {
+	$response = array("success" => false, "message" => "This email id is already registered with us!");
+    echo json_encode($response);
+    return;
+	// echo "This email id is already registered with us!";
+	// exit;
+}
+
+$sql = "INSERT INTO users (email, password, full_name, phone, gender, college_name) VALUES ('$email', '$password', '$full_name', '$phone', '$gender', '$college_name')";
+$result = mysqli_query($conn, $sql);
+if (!$result) {
+	$response = array("success" => false, "message" => "Something went wrong!");
+    echo json_encode($response);
+    return;
+	// echo "Something went wrong!";
+	// exit;
+}
+
+$response = array("success" => true, "message" => "Your account has been created successfully!");
+echo json_encode($response);
+mysqli_close($conn);
+
+
+// echo "Your account has been created successfully!";
+// ?>
+
+// Click <a href="../index.php">here</a> to continue.
+// <?php
+// mysqli_close($conn);
